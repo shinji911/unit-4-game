b0512eedb2007187b16807190986c43dd1ed4917
kshinji911 <kshinji911@gmail.com>
2018-12-07 21:39:59 -0500

    htmldone

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ assignment 4
 
 4 Fate servant characters.
 
-4 objects that hold their class, name, hp, atk power, counter power. image src.
+4 objects that hold their name, hp, atk power, counter power.
+
+4 clickable divs that show their name, picture, current hp.
+
+once playerchar selected, that div cannot be clicked anymore. move rest of divs to enemysvt div.
+
+once fight target selected, none of the divs can be clicked until fight is over.
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,39 @@
+//ready js
+$(document).ready(function () {
+
 // initialize variables
 let 
 
 //character objects
 let archerobj = {
-    name : 
-}
+    name : "Arash",
+    hp : 100,
+    atkpower : 10,
+    ctrpower : 25
+};
+
+let casterobj = {
+    name : "William Shakespeare",
+    hp : 100,
+    atkpower : 10,
+    ctrpower : 25
+};
+
+let lancerobj = {
+    name : "Karna",
+    hp : 100,
+    atkpower : 10,
+    ctrpower : 25
+};
+
+let riderobj = {
+    name : "Alexander",
+    hp : 100,
+    atkpower : 10,
+    ctrpower : 25
+};
+
+
+
+//end ready js
+});
